0ae246514ff4d1f3f6854b46a5b247cdfc14816a
GabrielJan02 <239590@vutbr.cz>
2025-05-05 22:51:35 +0200

    Update map.js

--- static/map.js
+++ static/map.js
@@ -8,8 +8,8 @@ fetch("/locations")
     .then(res => res.json())
     .then(locations => {
         locations.forEach(loc => {
-            // Pozor: tady musíš mít souřadnice, zatím tam dáme dummy, ale opravíme později
-            const marker = L.marker([0, 0]).addTo(map).bindPopup(`Načítám data pro ${loc.name}...`);
+            // Použij reálné souřadnice
+            const marker = L.marker([loc.lat, loc.lng]).addTo(map).bindPopup(`Načítám data pro ${loc.name}...`);
 
             marker.on('click', () => {
                 fetch(`/data/${loc.id}`)
@@ -32,30 +32,7 @@ fetch("/locations")
                     });
             });
         });
+    })
+    .catch(err => {
+        console.error("Chyba při načítání lokací:", err);
     });
-
-
-locations.forEach(loc => {
-    const marker = L.marker([loc.lat, loc.lng]).addTo(map).bindPopup(`Načítám data pro ${loc.name}...`);
-
-    marker.on('click', () => {
-        fetch(`/data/${loc.id}`)
-            .then(res => res.json())
-            .then(data => {
-                if (data.length === 0) {
-                    marker.setPopupContent(`<strong>${loc.name}</strong><br>Žádná data o krádežích.`);
-                } else {
-                    let content = `<strong>${loc.name}</strong><br><ul class="popup-list">`;
-                    data.forEach(row => {
-                        content += `<li>${row.znacka}: ${row.pocet}×</li>`;
-                    });
-                    content += '</ul>';
-                    marker.setPopupContent(content).openPopup();
-                }
-            })
-            .catch(err => {
-                marker.setPopupContent("Chyba při načítání dat.");
-                console.error(err);
-            });
-    });
-});
